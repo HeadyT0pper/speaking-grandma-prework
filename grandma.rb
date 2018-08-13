d79b12ec3563d8b3b9aa7c0c6886880e4d4273d1
Jonathan Stricklin <jstricklin89@gmail.com>
2018-08-13 17:24:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.downcase
+    puts "HUH?! SPEAK UP SONNY!"
   end
 end
 # phrase != phrase.upcase
